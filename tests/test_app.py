63966b74fad349427caf6c7b4b8570d75bd5841b
Nguyen Thanh Sang <sang211nt@gmail.com>
2024-07-15 22:20:00 +0700

    update the test code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
-import unittest
-import json
+import sys
+import os
+import pytest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
 class OrdersTestCase(unittest.TestCase):
